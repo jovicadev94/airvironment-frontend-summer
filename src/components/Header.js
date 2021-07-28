a56f26b3f443b18e2ba077f3129b673bced36c47
jovicadev94 <jovica.misirlic@gmail.com>
2021-07-28 12:37:36 +0200

    Add routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "../assets/styles/components/Header.scss";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="header">
-      <div className="logo">
-        <span className="header-span-opacity">air</span>
-        <span className="nav2">vironment</span>
+      <div className="home-navigation">
+        <Link to="/">
+          <span className="header-span-opacity">air</span>
+          <span className="nav2">vironment</span>
+        </Link>
       </div>
-      <div className="all_data">all data</div>
-      <div className="averages"> averages</div>
+      <div className="navigation">
+        <span className="all_data">all data</span>
+        <span className="averages"> averages</span>
+      </div>
     </div>
   );
 };
